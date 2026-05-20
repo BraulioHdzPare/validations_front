1942d6f494d1d4b1bd929d11f585dc3aec896374
Braulio Emmanuel Hernandez Martin <braulio.hmartin@alumnos.udg.mx>
2026-05-20 11:52:07 -0600

    refactorizar: cargar estilos desde head en html para evitar FOUC

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 import * as bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-import '../css/variables.css';
-import '../css/styles.css';
 
 window.bootstrap = bootstrap;
